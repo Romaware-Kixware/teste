e0903f3659c799901012c30423bb4523f6efb9a1
Flavius G <flaviusmaximus@gmail.com>
2023-06-26 12:09:03 +0300

    a patrulea din LOCAL

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ echo "<br>Modificat din Remote";
 echo "<br>Modificat din local Prima";
 
 
-$teste4 = "Al treilea commit din LOCAL in GitSERVER";
+$teste4 = "<br> Al treilea commit din LOCAL in GitSERVER";
 
 echo $teste4 ;
 
